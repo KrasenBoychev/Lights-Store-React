1b5a748ece3b2dc065140ffd94af6b69e5a900db
Krasen Boychev <krasenboychev11@gmail.com>
2024-07-01 11:10:36 +0100

    main components added

--- lights/src/App.jsx
+++ lights/src/App.jsx
@@ -1,8 +1,36 @@
-function App() {
+import About from './components/About';
+import Banner from './components/Banner';
+import Comments from './components/Comments';
+import ContactUs from './components/ContactUs';
+import Copyright from './components/Copyright';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Lights from './components/Lights';
+import Services from './components/Services';
+import WhoWeAre from './components/WhoWeAre';
 
+function App() {
   return (
     <>
-     <h1>Html Template</h1>
+      <Header />
+
+      <Banner />
+
+      <Services />
+
+      <About />
+
+      <Lights />
+
+      <WhoWeAre />
+
+      <Comments />
+
+      <ContactUs />
+
+      <Footer />
+
+      <Copyright />
     </>
   );
 }
