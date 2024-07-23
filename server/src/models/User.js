9d4ca95ccd4b0d2cca4c87824019f5b5fa20f36d
Krasen Boychev <krasenboychev11@gmail.com>
2024-07-23 17:22:32 +0100

    'cart' added to the User Model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -14,6 +14,10 @@ const UserSchema = new Schema({
         type: Date,
         default: Date.now,
     },
+    cart: {
+        type: Array,
+        default: []
+    }
 });
 const User = model('users', UserSchema);
 User.createIndexes();
