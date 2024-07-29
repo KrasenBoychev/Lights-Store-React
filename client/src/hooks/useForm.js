33efcafa1ae56d854a3c14058d5e3bdccc76a2ec
Krasen Boychev <krasenboychev11@gmail.com>
2024-07-29 11:15:21 +0100

    submit handler updated set the values to initial values

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -14,6 +14,8 @@ export function useForm(initialValues, submitCallback) {
       const submitHandler = (e) => {
         e.preventDefault();
         submitCallback(values);
+
+        setValues(initialValues);
       };
 
       return {
