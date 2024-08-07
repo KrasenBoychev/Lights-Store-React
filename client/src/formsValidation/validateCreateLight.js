b3a9aa9d100431c8c9449de540fc26362f42e831
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-07 08:42:45 +0100

    Updated validateCreateLight.js - number fields don't work with negative nums and 0

--- client/src/formsValidation/validateCreateLight.js
+++ client/src/formsValidation/validateCreateLight.js
@@ -4,6 +4,7 @@ export default function validateCreateLightForm(
   adjustable,
   integratedLed
 ) {
+  
   const allErrors = {};
 
   if (data.name == '') {
@@ -12,10 +13,18 @@ export default function validateCreateLightForm(
 
   if (data.price == '') {
     allErrors.price = 'Price field is required';
+  } else {
+    if (Number(data.price) <= 0) {
+      allErrors.price = 'Price field should be a positive number';
+    }
   }
 
   if (data.quantities == '') {
     allErrors.quantities = 'Quantities field is required';
+  } else {
+    if (Number(data.quantities) <= 0) {
+      allErrors.quantities = 'Quantities field should be a positive number';
+    }
   }
 
   if (data.date == '') {
@@ -36,6 +45,12 @@ export default function validateCreateLightForm(
 
     if (!result) {
       allErrors.dimensions = 'Dimensions is not valid';
+    } else {
+      const [h, w, d] = result[0].split('/');
+
+      if (Number(h) <= 0 || Number(w) <= 0 || Number(d) <= 0) {
+        allErrors.dimensions = 'Dimensions should consists of positive numbers';
+      }
     }
   }
 
@@ -46,10 +61,20 @@ export default function validateCreateLightForm(
   if (adjustable == true) {
     if (data.minHeight == '' || data.minHeight == null) {
       allErrors.minHeight = 'Min field is required';
+    } else {
+      if (Number(data.minHeight) <= 0) {
+        allErrors.minHeight = 'Min field should be a positive number';
+      }
     }
 
     if (data.maxHeight == '' || data.maxHeight == null) {
       allErrors.maxHeight = 'Max field is required';
+    } else if (Number(data.maxHeight) <= 0) {
+      allErrors.maxHeight = 'Max field should be a positive number';
+    } else {
+      if (data.minHeight && data.maxHeight <= data.minHeight) {
+        allErrors.maxHeight = 'Max height should be greater than min height';
+      }
     }
   } else {
     data.minHeight = '';
@@ -61,26 +86,42 @@ export default function validateCreateLightForm(
   } else if (integratedLed == true) {
     if (data.kelvins == '' || data.kelvins == null) {
       allErrors.kelvins = 'Kelvins field is required';
+    } else {
+      if (Number(data.kelvins) <= 0) {
+        allErrors.kelvins = 'Kelvins field should be a positive number';
+      }
     }
 
     if (data.lumens == '' || data.lumens == null) {
       allErrors.lumens = 'Lumens field is required';
+    } else {
+      if (Number(data.lumens) <= 0) {
+        allErrors.lumens = 'Lumens field should be a positive number';
+      }
     }
 
     if (data.watt == '' || data.watt == null) {
       allErrors.watt = 'Watt field is required';
+    } else {
+      if (Number(data.watt) <= 0) {
+        allErrors.watt = 'Watt field should be a positive number';
+      }
     }
 
     data.bulbType = '';
     data.bulbsRequired = '';
   } else {
-
     if (data.bulbType == '' || data.bulbType == null) {
       allErrors.bulbType = 'Bulb type field is required';
     }
 
     if (data.bulbsRequired == '' || data.bulbsRequired == null) {
       allErrors.bulbsRequired = 'Number of bulbs field is required';
+    } else {
+      if (Number(data.bulbsRequired) <= 0) {
+        allErrors.bulbsRequired =
+          'Number of bulbs field should be a positive number';
+      }
     }
 
     data.kelvins = '';
