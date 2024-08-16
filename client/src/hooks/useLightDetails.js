9514b2d048864a57e4cc9f34f3420c405fd06481
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-16 19:20:11 +0100

    useLight.js hook has validation for the light Id

--- client/src/hooks/useLightDetails.js
+++ client/src/hooks/useLightDetails.js
@@ -1,24 +1,51 @@
 import { useEffect, useState } from 'react';
-import {  useLocation, useParams } from 'react-router-dom';
+import {  useLocation, useNavigate, useParams } from 'react-router-dom';
 
 import { getLightById } from '../../api/lights-api';
+import { adminId } from '../../api/requester';
 
 import { useAuthContext } from '../contexts/AuthContext';
+import toast from 'react-hot-toast';
 
 export function useLightDetails() {
   const [spinner, setSpinner] = useState(false);
   const [light, setLight] = useState({});
+
   const { lightId } = useParams();
   const location = useLocation();
+  const navigate = useNavigate();
+
+  const { userId, userCart } = useAuthContext();
+
   const currPage = location.pathname.split('/')[1];
  
   useEffect(() => {
     (async function getLight() {
       try {
         setSpinner(true);
-        const getlight = await getLightById(lightId);
+        const getLight = await getLightById(lightId);
 
-        setLight(getlight);
+        if (currPage == 'catalog' && getLight.ownerId != adminId) {
+          toast.error('Access Denied');
+          navigate('/catalog');
+        }
+
+        if (currPage == 'marketplace' && (getLight.ownerId == adminId || getLight.ownerId == userId)) {
+          toast.error('Access Denied');
+          navigate('/marketplace');
+        }
+
+        if (currPage == 'profile' && getLight.ownerId != userId) {
+          toast.error('Access Denied');
+          navigate('/profile');
+        }
+
+        if (currPage == 'cart' && !userCart.includes(getLight._id)) {
+          toast.error('Access Denied');
+          navigate('/cart');
+        }
+
+        setLight(getLight);
       } catch (error) {
         alert(error.message);
       } finally {
