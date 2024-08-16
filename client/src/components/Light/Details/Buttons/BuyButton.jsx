c5a7b5925c56302aa5cf23656b10746b85bb51b0
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-16 19:18:42 +0100

    when Guest clicks Buy Button 'navigate' provide the location to the Login component

--- client/src/components/Light/Details/Buttons/BuyButton.jsx
+++ client/src/components/Light/Details/Buttons/BuyButton.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import toast from 'react-hot-toast';
+import { useLocation } from 'react-router-dom';
 
 import { addToCart } from '../../../../../api/cart-api';
 
@@ -8,11 +9,12 @@ import { useAuthContext } from '../../../../contexts/AuthContext';
 export default function BuyButton({ props }) {
   const { light, setBoughtItem, navigate } = props;
 
+  const location = useLocation();
   const authData = useAuthContext();
 
   const buyClickHandler = async () => {
     if (!authData.userId) {
-      navigate('/login');
+      navigate('/login', {state: location.pathname});
       return;
     }
 
