e8ff60c54fa163928771d10cbb7e0519e6e0c767
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-17 14:34:34 +0100

    Created Sort component

---
+++ client/src/components/ShowLights/chunks/Sort.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+export default function Sort({ props }) {
+  const { sort, setSort } = props;
+
+  const changeHandler = (e) => {
+    setSort(e.target.value);
+  };
+
+  return (
+    <div className="sort-lights">
+      <select name="sortLights" value={sort} onChange={changeHandler}>
+        <option value="nameAscending">A-Z</option>
+        <option value="nameDescending">Z-A</option>
+        <option value="priceAscending">Price Up</option>
+        <option value="priceDescending">Price Down</option>
+      </select>
+    </div>
+  );
+}
