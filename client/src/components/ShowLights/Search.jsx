2d9f790629db84cae36c54c2d2c376281f78db6f
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-17 10:50:19 +0100

    Search component renders lights using a hook and listens for a change rather than listening for a click event

--- client/src/components/ShowLights/Search.jsx
+++ client/src/components/ShowLights/Search.jsx
@@ -1,60 +1,90 @@
 /* eslint-disable react/prop-types */
-
-import { useForm } from '../../hooks/useForm';
+import { useEffect } from 'react';
 
 export default function Search(props) {
   const { lightsState, filteredLightsState, searchFormProps } = props;
 
-  const searchFormClickHandler = (data) => {
+  const { seacrhFormValues, setSearchFormValues } = searchFormProps;
+  const { name, minPrice, maxPrice, lightType } = seacrhFormValues;
 
-    const { name, price, lightType } = data;
-    const priceToNum = Number(price);
+  const changeHandler = (e) => {
+    setSearchFormValues((state) => ({
+      ...state,
+      [e.target.name]: e.target.value != 'defaultValue' ?  e.target.value : ''
+    }));
+  };
 
+  useEffect(() => {
+    (function search() {
     const filteredItems = lightsState.lights.filter(
       (light) =>
-        light.name.toLowerCase().includes(name.toLowerCase()) &&
-        (price ? light.price === priceToNum : true) &&
-        (lightType == 'integratedLed'
-          ? light.kelvins
-          : lightType == 'bulbType'
-          ? light.bulbType
-          : true)
+        light.name.toLowerCase().includes(name.toLowerCase()) 
+          && (Number(minPrice) 
+              ? Number(maxPrice)
+                ? light.price >= Number(minPrice) && light.price <= Number(maxPrice)
+                : light.price >= Number(minPrice)
+              : Number(maxPrice)
+                ? light.price <= Number(maxPrice)
+                : true
+             )
+          && (lightType == 'integratedLed'
+              ? light.kelvins
+              : lightType == 'bulbType'
+                  ? light.bulbType
+                  : true
+             )
     );
 
     filteredLightsState.setFilteredLights(filteredItems);
+  })();
+
+  }, [name, minPrice, maxPrice, lightType]);
+
+  const clearAllFields = (e) => {
+    e.preventDefault();
+
+    setSearchFormValues({
+      name: '',
+      minPrice: '',
+      maxPrice: '',
+      lightType: ''
+    });
   };
 
-  const { values, changeHandler, submitHandler } = useForm(
-    searchFormProps.seacrhFormValues,
-    searchFormClickHandler
-  );
-
   return (
-    <form onSubmit={submitHandler} className="marketplace-search-form">
+    <form className="marketplace-search-form">
       <input
         type="text"
         name="name"
         placeholder="Name"
-        value={values.name}
+        value={name}
         onChange={changeHandler}
       />
       <input
         type="number"
-        name="price"
-        placeholder="Price"
-        value={values.price}
+        name="minPrice"
+        placeholder="Min Price"
+        value={minPrice}
+        onChange={changeHandler}
+      />
+       <input
+        type="number"
+        name="maxPrice"
+        placeholder="Max Price"
+        value={maxPrice}
         onChange={changeHandler}
       />
       <select
         name="lightType"
-        value={values.lightType}
+        value={lightType}
         onChange={changeHandler}
       >
         <option value="defaultValue">--- Type of Light ---</option>
         <option value="integratedLed">Integrated LED</option>
         <option value="bulbType">Bulb Type</option>
       </select>
-      <button>Filter</button>
+
+      <button onClick={clearAllFields}>Clear</button>
     </form>
   );
 }
