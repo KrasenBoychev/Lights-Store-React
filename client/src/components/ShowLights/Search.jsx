bdc9e00b287eba16f088af391e1ac0d4e1e6476a
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-17 11:02:31 +0100

    Search component uses useSearch hook

--- client/src/components/ShowLights/Search.jsx
+++ client/src/components/ShowLights/Search.jsx
@@ -1,11 +1,8 @@
 /* eslint-disable react/prop-types */
-import { useEffect } from 'react';
+import { useSearch } from '../../hooks/useSearch';
 
 export default function Search(props) {
-  const { lightsState, filteredLightsState, searchFormProps } = props;
-
-  const { seacrhFormValues, setSearchFormValues } = searchFormProps;
-  const { name, minPrice, maxPrice, lightType } = seacrhFormValues;
+  const [name, minPrice, maxPrice, lightType, setSearchFormValues] = useSearch(props);
 
   const changeHandler = (e) => {
     setSearchFormValues((state) => ({
@@ -13,32 +10,6 @@ export default function Search(props) {
       [e.target.name]: e.target.value != 'defaultValue' ?  e.target.value : ''
     }));
   };
-
-  useEffect(() => {
-    (function search() {
-    const filteredItems = lightsState.lights.filter(
-      (light) =>
-        light.name.toLowerCase().includes(name.toLowerCase()) 
-          && (Number(minPrice) 
-              ? Number(maxPrice)
-                ? light.price >= Number(minPrice) && light.price <= Number(maxPrice)
-                : light.price >= Number(minPrice)
-              : Number(maxPrice)
-                ? light.price <= Number(maxPrice)
-                : true
-             )
-          && (lightType == 'integratedLed'
-              ? light.kelvins
-              : lightType == 'bulbType'
-                  ? light.bulbType
-                  : true
-             )
-    );
-
-    filteredLightsState.setFilteredLights(filteredItems);
-  })();
-
-  }, [name, minPrice, maxPrice, lightType]);
 
   const clearAllFields = (e) => {
     e.preventDefault();
@@ -57,26 +28,26 @@ export default function Search(props) {
         type="text"
         name="name"
         placeholder="Name"
-        value={name}
+        value={name ? name : ''}
         onChange={changeHandler}
       />
       <input
         type="number"
         name="minPrice"
         placeholder="Min Price"
-        value={minPrice}
+        value={minPrice ? minPrice : ''}
         onChange={changeHandler}
       />
        <input
         type="number"
         name="maxPrice"
         placeholder="Max Price"
-        value={maxPrice}
+        value={maxPrice ? maxPrice : ''}
         onChange={changeHandler}
       />
       <select
         name="lightType"
-        value={lightType}
+        value={lightType ? lightType : ''}
         onChange={changeHandler}
       >
         <option value="defaultValue">--- Type of Light ---</option>
