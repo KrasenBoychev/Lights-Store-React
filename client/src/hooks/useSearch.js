4dd8dffd28510bcf1d912174deb298fc6e5f1126
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-17 11:02:14 +0100

    Created useSearch.js hook

---
+++ client/src/hooks/useSearch.js
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+
+export function useSearch(props) {
+    const { lightsState, filteredLightsState, searchFormProps } = props;
+    const { seacrhFormValues, setSearchFormValues } = searchFormProps;
+    const { name, minPrice, maxPrice, lightType } = seacrhFormValues;
+
+    useEffect(() => {
+        (function search() {
+        const filteredItems = lightsState.lights.filter(
+          (light) =>
+            light.name.toLowerCase().includes(name.toLowerCase()) 
+              && (Number(minPrice) 
+                  ? Number(maxPrice)
+                    ? light.price >= Number(minPrice) && light.price <= Number(maxPrice)
+                    : light.price >= Number(minPrice)
+                  : Number(maxPrice)
+                    ? light.price <= Number(maxPrice)
+                    : true
+                 )
+              && (lightType == 'integratedLed'
+                  ? light.kelvins
+                  : lightType == 'bulbType'
+                      ? light.bulbType
+                      : true
+                 )
+        );
+    
+        filteredLightsState.setFilteredLights(filteredItems);
+      })();
+    
+      }, [name, minPrice, maxPrice, lightType]);
+
+
+    return [name, minPrice, maxPrice, lightType, setSearchFormValues];
+}
