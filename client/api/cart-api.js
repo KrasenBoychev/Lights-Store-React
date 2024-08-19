1d8dcb5158c98c96e5715d90229b76c7d6437bc1
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-19 10:42:25 +0100

    Created new cart-api request

--- client/api/cart-api.js
+++ client/api/cart-api.js
@@ -6,8 +6,13 @@ export async function addToCart(lightId) {
     return await api.put(host + '/cart/' + lightId);
 }
 
-export async function getCart(userId) { 
-    return await api.get(host + '/cart/' + userId); 
+export async function updateUserCart(lightsIds) {
+    if (lightsIds.length == 0) {
+        return await api.put(host + '/cart/userCart/' + 'noLights');
+    } else {
+        return await api.put(host + '/cart/userCart/' + lightsIds);
+    }
+   
 }
 
 export async function getCartLights(lightsId) {
