eec781b57c240e2a7ef515ec3f1ed2e1d881ea18
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-19 10:43:51 +0100

    Cart.jsx uses userCart from useAuthContext hook to render lights

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,20 +1,20 @@
-import useCart from '../../hooks/useCart';
+import { useAuthContext } from '../../contexts/AuthContext';
+import { useCart } from '../../hooks/useCart';
 
 import CatalogLight from '../Light/CatalogLight/CatalogLight';
-import Spinner from '../Spinner';
 
 export default function Cart() {
-  const [cart, setCart, spinner] = useCart();
+  const { userCart } = useAuthContext();
+
+  useCart();
 
   return (
     <div className="catalog_section">
       <h1>Your Cart</h1>
 
       <div className="items-container">
-        {spinner ? (
-          <Spinner />
-        ) : cart.length > 0 ? (
-          cart.map((light) => {
+        {userCart.length > 0 ? (
+          userCart.map((light) => {
             return <CatalogLight key={light._id} {...light} />;
           })
         ) : (
