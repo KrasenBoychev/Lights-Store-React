62f77f7641911ec8612a844902fe4ce4fd78ad23
Krasen Boychev <krasenboychev11@gmail.com>
2025-01-25 10:33:09 +0000

    website deployed

--- server/src/config/configDatabase.js
+++ server/src/config/configDatabase.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 
 function connectDatabase() {
+  // mongoose
+  //   .connect('mongodb://localhost:27017/', {
+  //     dbName: 'lights-store'
+  //   })
+
+  //mongodb+srv://KrasenBoychev:<db_password>@cluster0.zovge.mongodb.net/
+
   mongoose
-    .connect('mongodb://localhost:27017/', {
-      dbName: 'lights-store'
-    })
+  .connect('mongodb+srv://KrasenBoychev:KrasenBoychev@cluster0.zovge.mongodb.net/', {
+    dbName: 'lights-store'
+  })
+
     // eslint-disable-next-line no-unused-vars
     .then((res) => {
       console.log('Connected to your database');
