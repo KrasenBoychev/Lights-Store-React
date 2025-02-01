197d34133c5c55d0e9fdce7ab7b9ef44523ffe10
Krasen Boychev <krasenboychev11@gmail.com>
2025-02-01 20:08:36 +0000

    OurStore.jsx created and completed

---
+++ client/src/components/Our Store/OurStore.jsx
@@ -0,0 +1,21 @@
+import {
+  APIProvider,
+  Map,
+} from '@vis.gl/react-google-maps';
+import { GOOGLE_MAPS_API_KEY } from '../../../api/api-keys';
+import './ourStore.css';
+
+export default function OurStore() {
+  return (
+    <div className="our_store_section">
+      <APIProvider
+        apiKey={GOOGLE_MAPS_API_KEY}
+      >
+        <Map
+          defaultZoom={13}
+          defaultCenter={{ lat: 43.204666, lng: 27.910543 }}
+        ></Map>
+      </APIProvider>
+    </div>
+  );
+}
