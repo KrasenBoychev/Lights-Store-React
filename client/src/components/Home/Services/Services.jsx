12c91897cf16c597094d325ae36e446376fadb96
Krasen Boychev <krasenboychev11@gmail.com>
2025-02-01 21:28:14 +0000

    Services.jsx updated

--- client/src/components/Home/Services/Services.jsx
+++ client/src/components/Home/Services/Services.jsx
@@ -1,41 +1,35 @@
-import ServicesModel from './ServicesModel';
+import ServicesModel from "./ServicesModel";
+import "./services.css";
 
 export default function Services() {
   const servicesInfo = {
-    furnitureStore: {
-      img: 'images/icon-1.png',
-      heading: 'Furniture',
-      description:
-        'Take 10% off for Furniture Store if you spent 500lv. or more',
-    },
     design: {
-      img: 'images/icon-2.png',
-      heading: 'Design',
-      description: 'We will create a 3D project for you',
+      img: "/src/components/Home/Services/images/icon-2.png",
+      img_alt: "Design_Icon",
+      heading: "Design",
+      description: "We will create a 3D project for you",
     },
     delivery: {
-      img: 'images/icon-3.png',
-      heading: 'Delivery',
-      description: 'Free Home Delivery for orders over 100lv.',
+      img: "/src/components/Home/Services/images/icon-3.png",
+      img_alt: "Delivery_Icon",
+      heading: "Delivery",
+      description: "Free Home Delivery for orders over 100lv.",
     },
     installation: {
-      img: 'images/icon-4.png',
-      heading: 'Installation',
-      description: 'Our Qualified Electricians can install your lights',
+      img: "/src/components/Home/Services/images/icon-4.png",
+      img_alt: "Installation_Icon",
+      heading: "Installation",
+      description: "Our Qualified Electricians can install your lights",
     },
   };
 
   return (
-    <div className="services_section layout_padding">
-      <div className="container">
-        <h1 className="services_taital">Our Services</h1>
-        <div className="services_section2 layout_padding">
-          <div className="row">
-            {Object.entries(servicesInfo).map((service) => {
-              return <ServicesModel key={service[0]} service={service[1]} />;
-            })}
-          </div>
-        </div>
+    <div className="services_container">
+      <h2>Services</h2>
+      <div className="services_description">
+        {Object.entries(servicesInfo).map((service) => {
+          return <ServicesModel key={service[0]} service={service[1]} />;
+        })}
       </div>
     </div>
   );
