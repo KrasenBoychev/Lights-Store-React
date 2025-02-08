903406a3d9b69847a05d12c12bdde516350987aa
Krasen Boychev <krasenboychev11@gmail.com>
2025-02-08 10:06:46 +0000

    Comments.jsx completed

--- client/src/components/Home/Comments/Comments.jsx
+++ client/src/components/Home/Comments/Comments.jsx
@@ -9,6 +9,7 @@ export default function Comments() {
 
   const [commentOneIndex, setCommentOneIndex] = useState(0);
   const [commentTwoIndex, setCommentTwoIndex] = useState(1);
+  const [activeCommentsIndex, setActiveCommentsIndex] = useState(0);
   const [listItemsArr, setListItemsArr] = useState([]);
 
   useEffect(() => {
@@ -16,31 +17,40 @@ export default function Comments() {
 
     for (let index = 0; index < 8; index += 2) {
       listItems.push(
-        <li key={index} onClick={() => setNewComments(index, index + 1)}></li>
+        <li
+          key={index}
+          onClick={() => setNewComments(index, index + 1)}
+          className={
+            index === activeCommentsIndex
+              ? "active_comment"
+              : "no_active_comment"
+          }
+        ></li>
       );
     }
-
+  
     setListItemsArr(listItems);
-  }, []);
+  }, [activeCommentsIndex]);
 
   const setNewComments = (indexOne, indexTwo) => {
     setCommentOneIndex(indexOne);
     setCommentTwoIndex(indexTwo);
+    setActiveCommentsIndex(indexOne);
   };
 
   return (
     <div className="comments_container">
       <h2>What our customers say</h2>
-      <section>
+      <section className="comment_model_container">
         {comments.length > 0 ? (
           <CommentModel
             renderTwoComments={{ comments, commentOneIndex, commentTwoIndex }}
           />
         ) : (
-          "No comments yet"
+          <p className="no_comments_yet">No comments yet</p>
         )}
       </section>
-      <section>
+      <section className="comments_list_btns">
         <ul>{listItemsArr}</ul>
       </section>
     </div>
