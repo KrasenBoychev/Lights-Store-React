002f467aabf4d0b6b29834298b3ccfab5e7fa985
Krasen Boychev <krasenboychev11@gmail.com>
2025-02-08 10:06:27 +0000

    CommentModel.jsx completed

--- client/src/components/Home/Comments/CommentModel.jsx
+++ client/src/components/Home/Comments/CommentModel.jsx
@@ -2,10 +2,29 @@ import "./comments.css";
 
 export default function CommentModel({ renderTwoComments }) {
   const { comments, commentOneIndex, commentTwoIndex } = renderTwoComments;
+  const firstComment = comments[commentOneIndex];
+  const secondComment = comments[commentTwoIndex];
+
   return (
     <>
-      <p>{comments[commentOneIndex].name}</p>
-      <p>{comments[commentTwoIndex].name}</p>
+      <div className="comment_wrapper">
+        <div className="comment_author_image">
+          <img src={firstComment.imageURL} alt="comment_author_image" />
+        </div>
+        <div className="comment_text">
+          <h4>{firstComment.name}</h4>
+          <p>{firstComment.customerComment}</p>
+        </div>
+      </div>
+      <div className="comment_wrapper">
+        <div className="comment_author_image">
+          <img src={secondComment.imageURL} alt="comment_author_image" />
+        </div>
+        <div className="comment_text">
+          <h4>{secondComment.name}</h4>
+          <p>{secondComment.customerComment}</p>
+        </div>
+      </div>
     </>
   );
 }
