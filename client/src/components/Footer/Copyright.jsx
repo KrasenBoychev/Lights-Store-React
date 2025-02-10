162b7803756e1d56209c09c9307bf15940cf289a
Krasen Boychev <krasenboychev11@gmail.com>
2025-02-10 09:05:27 +0000

    copyright text changed

--- client/src/components/Footer/Copyright.jsx
+++ client/src/components/Footer/Copyright.jsx
@@ -1,13 +1,10 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Copyright() {
   return (
     <div className="copyright_section">
       <div className="container">
-        <p className="copyright_text">
-          2020 All Rights Reserved. Design by{' '}
-          <Link to="https://html.design">Free html Templates</Link>
-        </p>
+        <p className="copyright_text"> &copy; Lights Store 2024 created by Krasen Boychev</p>
       </div>
     </div>
   );
