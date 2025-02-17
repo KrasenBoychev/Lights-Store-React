3440df172f8fb1ac3c4ce5ba8ef31eb83bc3d08c
Krasen Boychev <krasenboychev11@gmail.com>
2025-02-17 10:35:14 +0000

    structure and classnames updated

--- client/src/components/Footer/Copyright.jsx
+++ client/src/components/Footer/Copyright.jsx
@@ -1,11 +1,9 @@
-import { Link } from "react-router-dom";
+import "./footer.css";
 
 export default function Copyright() {
   return (
-    <div className="copyright_section">
-      <div className="container">
-        <p className="copyright_text"> &copy; Lights Store 2024 created by Krasen Boychev</p>
-      </div>
-    </div>
+    <section className="copyright_section">
+      <p>&copy; Lights Store 2024 created by Krasen Boychev</p>
+    </section>
   );
 }
