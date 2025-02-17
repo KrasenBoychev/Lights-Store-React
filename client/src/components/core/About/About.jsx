7232cab5e94ba512ccd3daed8d7598509a4a4cd6
Krasen Boychev <krasenboychev11@gmail.com>
2025-02-17 14:44:08 +0000

    About.jsx new structure and classnames added

--- client/src/components/core/About/About.jsx
+++ client/src/components/core/About/About.jsx
@@ -1,37 +1,34 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "./about.css";
 
 export default function About() {
   return (
-    <div className="about_section layout_padding">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <h1 className="about_text">About Us</h1>
-            <p className="lorem_text">
-              The company was found in 1997 in Varna, where we opened our first
-              store. We started with a team of 5 people and now we are a team of
-              50 people with 10 stores located in 10 different cities and towns.
-            </p>
-            <p className="lorem_text">
-              Our mission is to offer excellent quality lights and services
-              which support our customers through the process of creating a
-              project to the installation. Our range of lights offer some of the
-              most popular styles such as modern, traditional, industrial and
-              vintage. You will find different combinations of colours and
-              designs as we always try to offer unusual style which will make
-              your home, office or commercial place looks amazing!
-            </p>
-            <div className="read_bt1">
-              <Link to={'/catalog'}>Lights</Link>
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="image_1">
-              <img src="images/bulb-background.jpg" />
-            </div>
-          </div>
+    <div className="about_container">
+      <section className="about_text">
+        <h1>About Us</h1>
+        <p>
+          The company was found in 1997 in Varna, where we opened our first
+          store. We started with a team of 5 people and now we are a team of 50
+          people with 10 stores located in 10 different cities and towns.
+        </p>
+        <p>
+          Our mission is to offer excellent quality lights and services which
+          support our customers through the process of creating a project to the
+          installation. Our range of lights offer some of the most popular
+          styles such as modern, traditional, industrial and vintage. You will
+          find different combinations of colours and designs as we always try to
+          offer unusual style which will make your home, office or commercial
+          place looks amazing!
+        </p>
+        <div className="about_catalog_link">
+          <Link to={"/catalog"}>Lights</Link>
         </div>
-      </div>
+      </section>
+      <aside className="about_image">
+        <div>
+          <img src="images/bulb-background.jpg" />
+        </div>
+      </aside>
     </div>
   );
 }
