fe6c1ec3290c6e1d4d4c843a1b468f60612ac3ed
Krasen Boychev <krasenboychev11@gmail.com>
2025-02-18 21:18:20 +0000

    imports updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
-import {
-  getCatalogLights,
-  getMarketplaceLights,
-  getProfileLights,
-} from "../api/lights-api";
-
 import { AuthContextProvider } from "./contexts/AuthContext";
 
 import Header from "./components/core/Header/Header";
 import Home from "./components/core/Home/Home";
 import About from "./components/core/About/About";
-import ShowLights from "./components/Lights/LightsPage";
 import Details from "./components/Lights/Single Light/Light Details/Details";
 import CreateLight from "./components/Lights/Single Light/Create Light/CreateLight";
-import Cart from "./components/Cart/Cart";
+import Cart from "./components/Lights/Lights Pages/User Lights/Cart";
 import Login from "./components/authentication/Login";
 import Register from "./components/authentication/Register";
+import Logout from "./components/authentication/Logout";
 import Footer from "./components/core/Footer/Footer";
 import CommentForm from "./components/Create Comment/CommentForm";
 import NotFound from "./components/core/Not Found/NotFound";
-import Logout from "./components/authentication/Logout";
 import OurStore from "./components/Store Location/OurStore";
+import Catalog from "./components/Lights/Lights Pages/Commercial Lights/Catalog";
+import Marketplace from "./components/Lights/Lights Pages/Commercial Lights/Marketplace";
+import Profile from "./components/Lights/Lights Pages/User Lights/Profile";
 
 import ScrollTop from "./common/ScrollTop";
 import PrivateGuard from "./common/PrivateGuard";
@@ -42,15 +38,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route
-              path="/catalog"
-              element={<ShowLights getDataFunc={getCatalogLights} />}
-            />
+            <Route path="/catalog" element={<Catalog />} />
             <Route path="/catalog/:lightId" element={<Details />} />
-            <Route
-              path="/marketplace"
-              element={<ShowLights getDataFunc={getMarketplaceLights} />}
-            />
+            <Route path="/marketplace" element={<Marketplace />} />
             <Route path="/marketplace/:lightId" element={<Details />} />
             <Route path="/comment" element={<CommentForm />} />
             <Route path="/our-store" element={<OurStore />} />
@@ -63,10 +53,7 @@ function App() {
             <Route element={<PrivateGuard />}>
               <Route path="/createlight" element={<CreateLight />} />
               <Route path="/edit/:lightId" element={<CreateLight />} />
-              <Route
-                path="/profile"
-                element={<ShowLights getDataFunc={getProfileLights} />}
-              />
+              <Route path="/profile" element={<Profile />} />
               <Route path="/profile/:lightId" element={<Details />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/cart/:lightId" element={<Details />} />
