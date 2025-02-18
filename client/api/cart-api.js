ec83db4bb5cd52fbf3bd4c9db596b86974ab40cf
Krasen Boychev <krasenboychev11@gmail.com>
2025-02-18 20:59:59 +0000

    not used code commented

--- client/api/cart-api.js
+++ client/api/cart-api.js
@@ -6,14 +6,15 @@ export async function addToCart(lightId) {
     return await api.put(host + '/cart/' + lightId);
 }
 
-export async function updateUserCart(lightsIds) {
-    if (lightsIds.length == 0) {
-        return await api.put(host + '/cart/userCart/' + 'noLights');
-    } else {
-        return await api.put(host + '/cart/userCart/' + lightsIds);
-    }
+//TO DELETE
+// export async function updateUserCart(lightsIds) {
+//     if (lightsIds.length == 0) {
+//         return await api.put(host + '/cart/userCart/' + 'noLights');
+//     } else {
+//         return await api.put(host + '/cart/userCart/' + lightsIds);
+//     }
    
-}
+// }
 
 export async function getCartLights(lightsId) {
     return await api.get(host + '/cart/lights/' + lightsId);
--- client/src/hooks/useCart.js
+++ client/src/hooks/useCart.js
@@ -1,24 +1,23 @@
-import { useEffect, useState } from 'react';
-import toast from 'react-hot-toast';
+//TO DELETE
+// import { useEffect, useState } from "react";
+// import toast from "react-hot-toast";
 
-import { updateUserCart } from '../../api/cart-api';
-import { useAuthContext } from '../contexts/AuthContext';
+// import { updateUserCart } from "../../api/cart-api";
+// import { useAuthContext } from "../contexts/AuthContext";
 
-export function useCart() {
-    const { userCart } = useAuthContext();
+// export function useCart() {
+//   const { userCart } = useAuthContext();
 
-    const [cart, setCart] = useState(userCart);
+//   const [cart, setCart] = useState(userCart);
 
-    useEffect(() => {
-        (async function updateCart() {
-            
-
-            try {
-                const lightsIds = cart.map(light => light._id);
-                await updateUserCart(lightsIds);
-            } catch(error) {
-                toast(error.message);
-            }
-        })();
-    });
-}
+//   useEffect(() => {
+//     (async function updateCart() {
+//       try {
+//         const lightsIds = cart.map((light) => light._id);
+//         await updateUserCart(lightsIds);
+//       } catch (error) {
+//         toast(error.message);
+//       }
+//     })();
+//   });
+// }
