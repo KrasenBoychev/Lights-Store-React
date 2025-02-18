57dc664a4114186ae5a7e2cac558bfcf2fe1ee0b
Krasen Boychev <krasenboychev11@gmail.com>
2025-02-18 21:09:59 +0000

    Cart.jsx uses RenederLights.jsx

---
+++ client/src/components/Lights/Lights Pages/User Lights/Cart.jsx
@@ -0,0 +1,22 @@
+import { useAuthContext } from "../../../../contexts/AuthContext";
+import RenderLights from "../shared components/RenderLights";
+
+import "../lightsPages.css";
+
+export default function Cart() {
+  const { userCart } = useAuthContext();
+
+  return (
+    <div className="lights_page_container">
+      <h1>Cart</h1>
+      <RenderLights
+        props={{
+          filteredLights: userCart,
+          currentItems: userCart,
+          spinner: false,
+          cartMsg: true,
+        }}
+      />
+    </div>
+  );
+}
