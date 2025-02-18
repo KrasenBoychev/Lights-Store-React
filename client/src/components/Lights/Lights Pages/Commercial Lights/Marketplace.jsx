e977f9d324cb9aa8c86af7d2679513e21e7b382e
Krasen Boychev <krasenboychev11@gmail.com>
2025-02-18 21:09:17 +0000

    Marketplace.jsx created

---
+++ client/src/components/Lights/Lights Pages/Commercial Lights/Marketplace.jsx
@@ -0,0 +1,43 @@
+import { useMarketplaceLights } from "../../../../hooks/lights/pages/useMarketplaceLights";
+import { useSortAndPaginate } from "../../../../hooks/lights/filter lights/useSortAndPaginate";
+
+import Search from "../../Filter Lights/Search";
+import RenderLights from "../shared components/RenderLights";
+import PaginateLights from "../shared components/PaginateLights";
+
+import "../lightsPages.css";
+
+export default function Marketplace() {
+  const [
+    lights,
+    setLights,
+    filteredLights,
+    setFilteredLights,
+    seacrhFormValues,
+    setSearchFormValues,
+    sort,
+    setSort,
+    spinner,
+  ] = useMarketplaceLights();
+
+  const [currentItems, pageCount, handlePageClick] = useSortAndPaginate(
+    filteredLights,
+    sort
+  );
+
+  return (
+    <div className="lights_page_container">
+      <h1>Marketplace Lights</h1>
+      <section className="lights_filter_wrapper">
+        <Search
+          lightsState={{ lights, setLights }}
+          filteredLightsState={{ filteredLights, setFilteredLights }}
+          searchFormProps={{ seacrhFormValues, setSearchFormValues }}
+          sortState={{ sort, setSort }}
+        />
+        <RenderLights props={{ filteredLights, currentItems, spinner }} />
+      </section>
+      <PaginateLights props={{ pageCount, handlePageClick }} />
+    </div>
+  );
+}
