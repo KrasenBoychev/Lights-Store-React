32beff47320b3ab4df3bba77a64ed7bc1e7b4ba1
Krasen Boychev <krasenboychev11@gmail.com>
2025-02-19 18:08:12 +0000

    OurLightsModel.jsx created as a reusable component within the OurLights folder

---
+++ client/src/components/core/Home/Our Lights/OurLightsModel.jsx
@@ -0,0 +1,28 @@
+import OverlaySearchIcon from "../../../reusable components/OverlaySearchIcon";
+
+export default function OurLightsModel({ props }) {
+  const { handlers, currState, imgProps, text, linkProps } = props;
+
+  return (
+    <section
+      className="our_lights_led"
+      onMouseOver={handlers.mouseOverHandler}
+      onMouseOut={handlers.mouseOutHandler}
+    >
+      <figure>
+        <img src={imgProps.imgPath} alt={imgProps.imgAlt} />
+        <figcaption>
+          <h3>{text.heading}</h3>
+          <p>{text.description}</p>
+        </figcaption>
+      </figure>
+
+      <OverlaySearchIcon
+        props={{
+          currState,
+          linkProps,
+        }}
+      />
+    </section>
+  );
+}
