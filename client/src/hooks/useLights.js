82af13d4fa46ee8b7ccb84094e8eec339e97edba
Krasen Boychev <krasenboychev11@gmail.com>
2025-02-24 18:44:04 +0000

    media css added for a screen size between 1080px and 1250px

--- client/src/hooks/useLights.js
+++ client/src/hooks/useLights.js
@@ -1,22 +1,18 @@
-import { useState, useEffect } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import toast from 'react-hot-toast';
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import toast from "react-hot-toast";
 
-import { useLogout } from './useAuth';
-import { getCatalogLights } from '../../api/lights-api';
-
-export function useAllLights({getLights}) {
+export function useAllLights({ getLights }) {
   const [spinner, setSpinner] = useState(false);
   const [lights, setLights] = useState([]);
   const [filteredLights, setFilteredLights] = useState([]);
   const [seacrhFormValues, setSearchFormValues] = useState({});
-  const [sort, setSort] = useState('nameAscending');
+  const [sort, setSort] = useState("nameAscending");
 
   const navigate = useNavigate();
-  const logout = useLogout();
 
   const location = useLocation();
-  const currPage = location.pathname.split('/')[1];
+  const currPage = location.pathname.split("/")[1];
 
   useEffect(() => {
     (async function getAllLights() {
@@ -27,29 +23,22 @@ export function useAllLights({getLights}) {
 
         if (location.state) {
           const filteredItems = allLights.filter((light) =>
-            location.state === 'integratedLed'
-              ? light.kelvins
-              : light.bulbType
+            location.state === "integratedLed" ? light.kelvins : light.bulbType
           );
           setFilteredLights(filteredItems);
-
         } else {
           setFilteredLights(allLights);
         }
 
         setSearchFormValues({
-          name: '',
-          minPrice: '',
-          maxPrice: '',
-          lightType: location.state
-            ? location.state 
-            : ''
+          name: "",
+          minPrice: "",
+          maxPrice: "",
+          lightType: location.state ? location.state : "",
         });
-        
       } catch (error) {
         toast(error.message);
-        navigate('/');
-        
+        navigate("/");
       } finally {
         setSpinner(false);
       }
@@ -63,7 +52,7 @@ export function useAllLights({getLights}) {
     setFilteredLights,
     seacrhFormValues,
     setSearchFormValues,
-    sort, 
+    sort,
     setSort,
     spinner,
     currPage,
